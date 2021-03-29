b886aafb20da890f396357fc93c1ed36d0c3756b
lexfor <thetim182001@mail.ru>
2021-03-29 19:41:58 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TestTaskCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestTaskCPP.dir/main.cpp.o"
+  "TestTaskCPP"
+  "TestTaskCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestTaskCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
